d13d4dd8399d6f8acd62e1d6c86fda4bd4bb8408
AnneVictoria <avduclaux@gmail.com>
2021-07-20 09:53:00 +0200

    add unique constraint

--- migrations/tables/02_update-reminder.sql
+++ migrations/tables/02_update-reminder.sql
@@ -1,1 +1,4 @@
 ALTER TABLE reminder ADD comment text;
+
+-- Ajoute une contrainte pour avoir des duos name/type uniques (pas de doublon)
+ALTER TABLE reminder ADD CONSTRAINT uniqueNameType UNIQUE (name, type)
